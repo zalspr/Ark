7ccbc4e4e0344dcca0b174473409f6b1f77e770f
crepls <40833618+crepls@users.noreply.github.com>
2022-06-11 10:44:38 +0300

    Update extenders.ts
    fix quotation mark

--- extensions/extenders.ts
+++ extensions/extenders.ts
@@ -10,7 +10,7 @@
                 let example = "✦ Basic Expressions: Add [+], Subtract [-], Multiply [∗], Divide[/] \n"
                 + "✦ Exponents [^2] and Square Roots [sqrt(2)] \n" 
                 + "✦ Logarithmic Functions [log(2)] \n"
-                + "✦ Trigonometry: Sine [sin(2)], Cosine [cos(2)], Tangent[tan(2)] \n
+                + "✦ Trigonometry: Sine [sin(2)], Cosine [cos(2)], Tangent[tan(2)] \n"
                 + "✦ Measurement Conversion: 2inch to cm, 4km to miles, etc. \n"
                 + "✦ Complex Expressions, and more." ;
 
